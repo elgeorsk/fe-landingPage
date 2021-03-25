d3ce82f9726eb8b5137696cdf35726b9b109e80f
ELGEOR-PC\elgeor <elgeor.sk@gmail.com>
2021-03-25 15:28:26 +0100

    Landing Page Behavior Navigation is built dynamically as an unordered list When clicking an item from the navigation menu, the link should scroll to the appropriate section.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Landing Page Project
+# 2. Landing Page Project
 
 ## Table of Contents
 
@@ -8,6 +8,6 @@
 
 The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
 
-To get started, open `js/app.js` and start building out the app's functionality
+To get started, open `assets/js/app.js` and start building out the app's functionality
 
 For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
--- assets/js/app.js
+++ assets/js/app.js
@@ -18,6 +18,10 @@
  * 
 */
 
+let sections = document.querySelectorAll('section');
+let menu = document.getElementById('navbar_list');
+let active = sections[0];
+let prActive;
 
 /**
  * End Global Variables
@@ -34,12 +38,43 @@
 */
 
 // build the nav
+// using for-in loop, i had an error in the console 'getAttribute is not a function'
+// solution - https://stackoverflow.com/questions/40835675/why-does-chrome-throw-getattribute-is-not-a-function-error-inside-a-for-loop
+for (let section = 0; section < sections.length; section++){
+    let aElement = document.createElement('a');
+    let liElement = document.createElement('li');
 
+    // set href value
+    aElement.href = '#'+ sections[section].id;
+
+    // set class
+    aElement.classList.add("menu_link");
+
+    // set text value
+    aElement.appendChild(document.createTextNode(sections[section].getAttribute('data-nav')));
+
+    menu.appendChild(liElement).appendChild(aElement);
+
+    // Scroll to anchor ID using scrollTO event
+    aElement.addEventListener("click", function (e) {
+        // https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
+        e.preventDefault();
+
+        // add active-class to section
+        prActive = active;
+        prActive.classList.remove('active_class');
+        active = sections[section];
+
+        active.classList.add('active_class');
+
+        // this refers to aElement
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: "smooth",
+        });
+    });
+}
 
 // Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
 
 
 /**
